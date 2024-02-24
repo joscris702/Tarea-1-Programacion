2848a6af068e338d0f0b89da335f547ac638b187
joscris702 <136862172+joscris702@users.noreply.github.com>
2024-02-23 21:47:11 -0600

    Add files via upload

---
+++ progra1/Program.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Fundamentos
+{
+    class Miprimerprograma
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hazlo asi, haz click");
+        }
+    }
+}
